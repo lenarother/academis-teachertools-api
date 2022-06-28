e674107e7c8d89ca784ee8998a1e02470586d7d0
Magdalena Rother <rother.magdalena@gmail.com>
2022-06-28 18:22:46 +0200

    Parse question

--- src/teachertools/markdown_edit/services.py
+++ src/teachertools/markdown_edit/services.py
@@ -1,2 +1,53 @@
-def split_markdown(data):
-    pass
+import re
+
+import markdown
+import markdownify
+
+ANSWERS_UL = r'^(.*?)<ul>(.*?)</ul>(.*?)$'
+ANSWERS_OL = r'^(.*?)<ol>(.*?)</ol>(.*?)$'
+ANSWER = r'<li>(.*?)</li>'
+
+
+def md2html(md):
+    return markdown.markdown(md.strip())
+
+
+def html2md(html):
+    return markdownify.markdownify(html.strip()).strip()
+
+
+def split_question_html(html):
+    for pattern in [ANSWERS_UL, ANSWERS_OL]:
+        if re.match(pattern, html, re.DOTALL):
+            before, answers, after = re.findall(ANSWERS_UL, html, re.DOTALL)[0]
+            return (
+                before.strip() or after.strip(),
+                answers
+            )
+    raise ValueError('No answers detected')
+
+
+def parse_answers_html(html):
+    return [html2md(a) for a in re.findall(ANSWER, html, re.DOTALL)]
+
+
+def parse_question(data):
+    """
+    0. Clean: strip
+    1. Parse md -> html
+    2. Extract answers list:
+        Find ul -> None: Find ol -> None: ValueError
+    3. Prepare question text: html -> md
+    4. Prepare answers list: split, remove li, html -> md
+
+    Return
+        - question text (md str)
+        - answers list (each answer as md str)
+    """
+    html = md2html(data)
+    text_html, answers_html = split_question_html(html)
+
+    text = html2md(text_html)
+    answer_list = parse_answers_html(answers_html)
+
+    return text, answer_list
--- src/testing/tests/markdown_edit/test_services.py
+++ src/testing/tests/markdown_edit/test_services.py
@@ -1,6 +1,6 @@
 import pytest
 
-from teachertools.markdown_edit.services import split_markdown
+from teachertools.markdown_edit.services import parse_question
 
 QUESTION_1 = """
 1) Which Python package would you like to learn more about?
@@ -11,10 +11,10 @@ QUESTION_1 = """
 - matplotlib
 """
 
-QUESTION_1_EXPECTED = [
-    'Which Python package would you like to learn more about?',
+QUESTION_1_EXPECTED = (
+    '1) Which Python package would you like to learn more about?',
     ['pandas', 'numpy', 'flask', 'matplotlib']
-]
+)
 
 QUESTION_2 = """
 2) Which of the following Python commands is correct?
@@ -25,15 +25,42 @@ QUESTION_2 = """
 * `print(hello world)`
 """
 
-QUESTION_2_EXPECTED = [
-    'Which of the following Python commands is correct?',
+QUESTION_2_EXPECTED = (
+    '2) Which of the following Python commands is correct?',
     [
         '`print hello world`',
         '`print(\'hello world\')`',
         '`print << \'hello world\'`',
         '`print(hello world)`',
     ]
-]
+)
+
+QUESTION_3 = """
+- pandas
+- numpy
+- flask
+- matplotlib
+"""
+
+QUESTION_3_EXPECTED = (
+    '',
+    ['pandas', 'numpy', 'flask', 'matplotlib']
+)
+
+QUESTION_4 = """
+- pandas
+- numpy
+- flask
+- matplotlib
+
+
+Which Python package would you like to learn more about?
+"""
+
+QUESTION_4_EXPECTED = (
+    'Which Python package would you like to learn more about?',
+    ['pandas', 'numpy', 'flask', 'matplotlib']
+)
 
 
 @pytest.mark.parametrize(
@@ -41,8 +68,9 @@ QUESTION_2_EXPECTED = [
     (
         (QUESTION_1, QUESTION_1_EXPECTED),
         (QUESTION_2, QUESTION_2_EXPECTED),
+        (QUESTION_3, QUESTION_3_EXPECTED),
+        (QUESTION_4, QUESTION_4_EXPECTED),
     )
 )
 def test_markdown_divided_into_parts(data, expected):
-    # assert split_markdown(data) == expected
-    split_markdown(data)
+    assert parse_question(data) == expected
